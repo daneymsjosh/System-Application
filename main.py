f7148a38e8ab66408369f27aa696fac7c62763d8
Joshua Janric Antonio <joshuajanric0170@gmail.com>
2024-03-30 12:15:15 +0800

    Updated joining of rooms
    Bugs:
    Usernames
    Connected messages

--- main.py
+++ main.py
@@ -28,6 +28,17 @@ def generate_unique_code(length):
     
     return code
 
+def update_rooms(snapshot, changes, read_time):
+    global rooms
+    for change in changes:
+        if change.type.name == "ADDED":
+            room_data = change.document.to_dict()
+            rooms[room_data["code"]] = {"members": 0, "messages": []}
+    print(rooms)
+
+rooms_ref = db.collection("Rooms")
+rooms_watch = rooms_ref.on_snapshot(update_rooms)
+
 @app.route("/", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
@@ -151,19 +162,19 @@ def connect(auth):
     rooms[room]["members"] += 1
     print(f"{name} joined room {room}")
 
-@socketio.on("disconnect")
-def disconnect():
-    room = session.get("room")
-    name = session.get("name")
-    leave_room(room)
+# @socketio.on("disconnect")
+# def disconnect():
+#     room = session.get("room")
+#     name = session.get("name")
+#     leave_room(room)
 
-    if room in rooms:
-        rooms[room]["members"] -= 1
-        if rooms[room]["members"] <= 0:
-            del rooms[room]
+#     if room in rooms:
+#         rooms[room]["members"] -= 1
+#         if rooms[room]["members"] <= 0:
+#             del rooms[room]
     
-    send({"name": name, "message": "has left the room"}, to=room)
-    print(f"{name} has left the room {room}")
+#     send({"name": name, "message": "has left the room"}, to=room)
+#     print(f"{name} has left the room {room}")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
